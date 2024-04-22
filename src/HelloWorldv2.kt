8f6d3f7f1b1f8b38685eb723603580ce47e684d9
timoteipiloiu <piloiutimotei@gmail.com>
2024-04-22 16:41:34 +0200

    HelloWorldv2

---
+++ src/HelloWorldv2.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    // Liest den Namen ein, und begrüßt den Nutzer
+    println("Hallo, wie heißt du? (confirm with Enter)")
+    var name = readln()
+    println("Hallo $name!")
+
+    // Druckt eine leere Zeile aus
+    println()
+
+    // Liest das Alter ein
+    println("Wie alt bist du? (confirm with Enter)")
+    var alter = readln()    // Diese Zeile liest das Alter ein
+    println("Wow! Bereits $alter Jahre?!")
+}
